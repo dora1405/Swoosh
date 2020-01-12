0f14a2acefccc613d14c3af1d2b629d6322b2a45
Ada <ada@Adas-MacBook-Air.local>
2020-01-11 23:41:34 -0800

    lesson 07

--- app/src/main/java/com/example/swoosh/SkillActivity.kt
+++ app/src/main/java/com/example/swoosh/SkillActivity.kt
@@ -5,11 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class SkillActivity : AppCompatActivity() {
+class SkillActivity : BaseActivity () {
+
+    var league = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
+
+        league = intent.getStringExtra(EXTRA_LEAGUE)
+        println(league)
     }
 
     fun leagueNextClicked(view: View){
